88b1057411403509dbb40e7d33728f0084b49b72
zhangswings <beforenight@163.com>
2016-01-17 20:20:09 +0800

    TextView点击事件

---
+++ app/src/main/java/com/example/zhangswings/practice/NextActivity.java
@@ -0,0 +1,40 @@
+package com.example.zhangswings.practice;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class NextActivity extends AppCompatActivity {
+TextView mTextView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_next);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        mTextView= (TextView) findViewById(R.id.textView);
+        mTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(NextActivity.this,"点击事件！！",Toast.LENGTH_LONG).show();
+            }
+        });
+
+
+    }
+
+}
